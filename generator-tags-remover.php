3f33ae1c9f6bed91d2980e8b13d00114a2a6cc30
Gulshan Thakare <gulshan@2020compute.com>
2018-06-27 17:18:43 +0530

    New version with version info remover

--- generator-tags-remover.php
+++ generator-tags-remover.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Generator Tags Remover
+Plugin Name: Generator Tags & Version Info Remover
 Plugin URI: 
-Description: It Removes generator tags of Wordpress, Revolution Slider, WPML, Visual Composer, WooCommerce.
-Version: 1.1
+Description: It Removes generator tags of Wordpress, Revolution Slider, WPML, Visual Composer, WooCommerce. It also removes the version info from the stylesheet.
+Version: 2.0
 Author: Gulshan Thakare
 Author URI: http://www.dmpmconcepts.com/
 GitHub Plugin URI: https://github.com/DMPMConcepts/generator-tags-remover
@@ -45,3 +45,18 @@ remove_action('wp_head', array(visual_composer(), 'addMetaData'));
 } 
 
 add_action('init', 'remove_generator_tags');
+
+// remove version info from Stylesheet
+
+function pkm_remove_appended_version_script_style( $target_url ) {
+    $filename_arr = explode('?', basename($target_url));
+    $filename = $filename_arr[0];
+        /* check if "ver=" argument exists in the url or not */
+        if(strpos( $target_url, 'ver=' )) {
+            $target_url = remove_query_arg( 'ver', $target_url );
+        }
+    return $target_url;
+}
+pkm_remove_appended_version_script_style( $url );
+
+add_filter('style_loader_src', 'pkm_remove_appended_version_script_style', 20000);
